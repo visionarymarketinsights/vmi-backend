00711395b6699511483cbc9f81576b04775ad38c
spandagle <siddhant.pandagle1998@gmail.com>
2023-10-17 18:50:43 +0530

    URL changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://fastapi-production-b9af.up.railway.app/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
