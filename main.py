e6edc64296fbbeecb0fba436c8e09ee3e416992b
spandagle <siddhant.pandagle1998@gmail.com>
2023-10-17 18:45:45 +0530

    Chat App

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-from fastapi import FastAPI
-from fastapi.websockets import WebSocket
-import httpx
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
 @app.get("/")
-async def root():
-    return {"greeting": "Git Changes!", "message": "Checking do git changes work!"}
-
-@app.get("/simple__http_call")
-async def make_http_call():
-    # Define the URL of the external API you want to call
-    external_api_url = "http://firstjumpuatservice.express-engg.com/api/Attendance/GetServerTime"
-
-    # Create an instance of the httpx.AsyncClient
-    async with httpx.AsyncClient() as client:
-        # Make an HTTP GET request to the external API
-        response = await client.get(external_api_url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response from the external API
-            data = response.json()
-            return {"data_from_external_api": data}
-        else:
-            # Handle error cases here
-            return {"error": "Failed to fetch data from the external API", "status_code": response.status_code}
-        
+async def get():
+    return HTMLResponse(html)
 
 
-@app.websocket("/ws/{chatroom_id}")
-async def websocket_endpoint(websocket: WebSocket, chatroom_id: str):
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
