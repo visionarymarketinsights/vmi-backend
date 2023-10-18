bc288eab6b6999a5773e11d54dc958963baefd22
spandagle <siddhant.pandagle1998@gmail.com>
2023-10-18 12:58:32 +0530

    MailJet added

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-from fastapi import FastAPI
-import httpx
+from fastapi import FastAPI, HTTPException
+from mailjet_rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ['MJ_APIKEY_PUBLIC']
+api_secret = os.environ['MJ_APIKEY_PRIVATE']
+
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"greeting": "Git Changes!", "message": "Checking do git changes work!"}
+    return {"greeting": "Hello!", "message": "This api works!"}
 
-@app.get("/simple__http_call")
-async def make_http_call():
-    # Define the URL of the external API you want to call
-    external_api_url = "http://firstjumpuatservice.express-engg.com/api/Attendance/GetServerTime"
 
-    # Create an instance of the httpx.AsyncClient
-    async with httpx.AsyncClient() as client:
-        # Make an HTTP GET request to the external API
-        response = await client.get(external_api_url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response from the external API
-            data = response.json()
-            return {"data_from_external_api": data}
-        else:
-            # Handle error cases here
-            return {"error": "Failed to fetch data from the external API", "status_code": response.status_code}
+@app.get("/send_email")
+async def send_email(subject:str, content:str):
+    data = {
+        'Messages': 
+        [
+            {
+            "From": {
+                "Email": "siddhant.pandagle1998@gmail.com",
+                "Name": "Siddhant Pandagle"
+            },
+            "To": [
+                {
+                "Email": "siddhant.pandagle1998@gmail.com",
+                "Name": "Siddhant Pandagle"
+                }
+            ],
+            "Subject": subject,
+            # "TextPart": "Greetings from Siddhant Pandagle!",
+            "HTMLPart": content
+            }
+        ]
+    }
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
+    return result.json()
