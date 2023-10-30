d28375fe3b0ad6a41141cf2895881a75eebb1cd2
sidpandagle <adi.pandagle@gmail.com>
2023-10-30 23:48:06 +0530

    Gmail SMTP added

--- app/routers/email.py
+++ app/routers/email.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI,  APIRouter
-from mailjet_rest import Client
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from fastapi import FastAPI,  APIRouter, HTTPException
 import os
 
 from pydantic import BaseModel
 
-api_key = os.environ["MJ_APIKEY_PUBLIC"]
-api_secret = os.environ["MJ_APIKEY_PRIVATE"]
-
-mailjet = Client(auth=(api_key, api_secret), version="v3.1")
+smtp_pass = os.environ["SIDSMTP"]
 
 
 class EmailRequest(BaseModel):
@@ -18,25 +17,23 @@ router = APIRouter()
 
 @router.post("/email")
 async def email(email_request:EmailRequest):
-    data = {
-        "Messages": [
-            {
-                "From": {
-                    "Email": "siddhant.pandagle1998@gmail.com",
-                    "Name": "Siddhant Pandagle",
-                },
-                "To": [
-                    {
-                        "Email": "siddhant.pandagle1998@gmail.com",
-                        "Name": "Siddhant Pandagle",
-                    }
-                ],
-                "Subject": email_request.subject,
-                "HTMLPart": email_request.content,
-            }
-        ]
-    }
-    result = mailjet.send.create(data=data)
-    print(result.status_code)
-    print(result.json())
-    return result.json()
+    try:
+        # Create a message
+        msg = MIMEMultipart()
+        msg["From"] = 'Siddhant <siddhant.pandagle1998@gmail.com>'
+        msg["To"] = 'siddhant.pandagle1998@gmail.com'
+        msg["Subject"] = email_request.subject
+
+        # Add the HTML message body
+        html_message = email_request.content
+        msg.attach(MIMEText(html_message, "html"))
+
+        # Create an SMTP server connection
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login('siddhant.pandagle1998', smtp_pass)
+            server.sendmail('Siddhant', 'siddhant.pandagle1998@gmail.com', msg.as_string())
+
+        return {"message": "Email sent successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
