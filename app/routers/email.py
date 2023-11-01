de31f5c3525094473449e2d851ca0b4245044bc1
spandagle <siddhant.pandagle1998@gmail.com>
2023-11-01 16:51:57 +0530

    Recaptcha check added

--- app/routers/email.py
+++ app/routers/email.py
@@ -1,8 +1,9 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-from fastapi import FastAPI,  APIRouter, HTTPException, Request
+from fastapi import FastAPI, APIRouter, HTTPException, Request
 import os
+import requests
 
 from pydantic import BaseModel
 
@@ -14,41 +15,62 @@ from slowapi.util import get_remote_address
 from ..utils.rate_limit import limiter
 
 smtp_pass = os.environ["SIDSMTP"]
+recaptcha_secret = os.environ["RECAPTCHA_SECRET_KEY"]
 
 
 class EmailRequest(BaseModel):
     subject: str
     content: str
+    response_token: str
+class RecaptchaResponse(BaseModel):
+    success: bool
+    message: str
+
 
 router = APIRouter()
 
 
 @router.get("/check_email")
-@limiter.limit("5/minute")
+@limiter.limit("20/minute")
 def email_check(request: Request):
     return {}
 
 
 @router.post("/email")
-@limiter.limit("5/minute")
-async def email(request: Request, email_request: EmailRequest):
+async def email(email_request: EmailRequest):
     try:
-        # Create a message
-        msg = MIMEMultipart()
-        msg["From"] = 'Siddhant <siddhant.pandagle1998@gmail.com>'
-        msg["To"] = 'siddhant.pandagle1998@gmail.com'
-        msg["Subject"] = email_request.subject
+        if not email_request.response_token:
+            return RecaptchaResponse(success=False, message='reCAPTCHA not completed.')
 
-        # Add the HTML message body
-        html_message = email_request.content
-        msg.attach(MIMEText(html_message, "html"))
+        verification_url = 'https://www.google.com/recaptcha/api/siteverify'
+        payload = {
+            'secret': recaptcha_secret,
+            'response': email_request.response_token,
+        }
+        response = requests.post(verification_url, data=payload)
 
-        # Create an SMTP server connection
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login('siddhant.pandagle1998', smtp_pass)
-            server.sendmail('Siddhant', 'siddhant.pandagle1998@gmail.com', msg.as_string())
+        if response.json()['success']:
+            # Create a message
+            msg = MIMEMultipart()
+            msg["From"] = "Siddhant <siddhant.pandagle1998@gmail.com>"
+            msg["To"] = "siddhant.pandagle1998@gmail.com"
+            msg["Subject"] = email_request.subject
 
-        return {"message": "Email sent successfully"}
+            # Add the HTML message body
+            html_message = email_request.content
+            msg.attach(MIMEText(html_message, "html"))
+
+            # Create an SMTP server connection
+            with smtplib.SMTP("smtp.gmail.com", 587) as server:
+                server.starttls()
+                server.login("siddhant.pandagle1998", smtp_pass)
+                server.sendmail(
+                    "Siddhant", "siddhant.pandagle1998@gmail.com", msg.as_string()
+                )
+
+            return {"message": "Email sent successfully"}
+        else:
+            return RecaptchaResponse(success=False, message='reCAPTCHA verification failed.')
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
