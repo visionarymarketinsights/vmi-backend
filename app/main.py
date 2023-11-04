1f0a551f18ffb86bc0e6c9f33668ce3cf42f38b1
fudge_fantastic <siddhant.pandagle1998@gmail.com>
2023-11-04 06:35:50 +0530

    Upload file done

--- app/main.py
+++ app/main.py
@@ -14,10 +14,15 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
+from fastapi.staticfiles import StaticFiles
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
 
 origins = [
     "http://localhost:3000",
--- app/routers/report.py
+++ app/routers/report.py
@@ -1,10 +1,15 @@
 # Import necessary modules and dependencies
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, File, UploadFile
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from app.models import Report  # Import the Report model
 from app.database import get_db
 from sqlalchemy import func
+import os
+import io
+from datetime import datetime
+from PIL import Image
+
 
 router = APIRouter()
 
@@ -76,3 +81,41 @@ async def delete_report(report_id: int, db: Session = Depends(get_db)):
     db.delete(report)
     db.commit()
     return {"message": "Report deleted"}
+
+
+@router.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        # Generate a timestamp
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        # Get the file extension
+        file_extension = file.filename.split(".")[-1]
+
+        # Specify the destination folder path
+        destination_folder = "images"
+        os.makedirs(destination_folder, exist_ok=True)
+
+        # Build the full file path including the timestamp and extension
+        file_path = os.path.join(destination_folder, f"{timestamp}.{file_extension}")
+
+        # Read the contents of the uploaded file
+        contents = file.file.read()
+
+        # Create a PIL Image object from the uploaded contents
+        image = Image.open(io.BytesIO(contents))
+
+        # Resize the image to your desired dimensions
+        # For example, resize the image to a maximum width of 800 pixels while maintaining the aspect ratio
+        max_width = 800
+        image.thumbnail((max_width, max_width))
+
+        # Save the compressed image to the specified file path
+        image.save(file_path)
+
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded and compressed {file.filename} to {file_path}"}
