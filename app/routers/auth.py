a4df8c4acfd4e14319b3e00c7e1f0573a380c662
sidpandagle <siddhant.pandagle1998@gmail.com>
2023-12-23 20:20:58 +0530

    Login Auth started

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,39 +6,43 @@ router = APIRouter()
 
 auth_handler = AuthHandler()
 users = [
+    {
+        "username": "admin",
+        "password": "$2b$12$3wxlo4o69vfgVrc4CKTg0ee/7.mmz2HTo1kjUfmssHsznUhMZyYee",
+    }
 ]
 
-@router.post('/register', status_code=201)
+
+@router.post("/register", status_code=201)
 def register(auth_details: AuthDetails):
-    if any(x['username'] == auth_details.username for x in users):
-        raise HTTPException(status_code=400, detail='Username is taken')
+    if any(x["username"] == auth_details.username for x in users):
+        raise HTTPException(status_code=400, detail="Username is taken")
     hashed_password = auth_handler.get_password_hash(auth_details.password)
-    users.append({
-        'username': auth_details.username,
-        'password': hashed_password    
-    })
-    return
+    users.append({"username": auth_details.username, "password": hashed_password})
+    return {"message": "Registration Success", "data": users[len(users) - 1]}
 
 
-@router.post('/login')
+@router.post("/login")
 def login(auth_details: AuthDetails):
     user = None
     for x in users:
-        if x['username'] == auth_details.username:
+        if x["username"] == auth_details.username:
             user = x
             break
-    
-    if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
-        raise HTTPException(status_code=401, detail='Invalid username and/or password')
-    token = auth_handler.encode_token(user['username'])
-    return { 'token': token }
+
+    if (user is None) or (
+        not auth_handler.verify_password(auth_details.password, user["password"])
+    ):
+        raise HTTPException(status_code=401, detail="Invalid username and/or password")
+    token = auth_handler.encode_token(user["username"])
+    return {"token": token}
 
 
-@router.get('/unprotected')
+@router.get("/unprotected")
 def unprotected():
-    return { 'hello': 'world' }
+    return {"hello": "world"}
 
 
-@router.get('/protected')
+@router.get("/protected")
 def protected(username=Depends(auth_handler.auth_wrapper)):
-    return { 'name': username }
+    return {"name": username}
